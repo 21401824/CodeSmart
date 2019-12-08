5cf182285dacc6694dbb45e99a46d8b1d4c72c61
Mfundo <makhandaemfene123@gmail.com>
2019-12-08 18:24:56 +0530

    My Second Java Program
    Enter a Number V2

---
+++ EnterNumber_V2.java
@@ -0,0 +1,39 @@
+package com.school;
+
+import java.util.Scanner;
+
+public class EnterNumber_V2 {
+
+	public static void main(String[] args) {
+		// Do while loop Code allowing a user to Enter a Number Until
+		Scanner scan =new Scanner(System.in);
+		int var=0;
+		do{
+				System.out.println("Enter Number:");
+				var=scan.nextInt();
+		}
+		while(var!=5);
+		
+			System.out.println("Var Found");
+		
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+
+
+}
